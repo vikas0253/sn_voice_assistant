0847d1a8d19a39889af6e2f69e1fb5834cb25681
Vikas Sakhare <vsakhare@qualys.com>
2019-03-21 14:05:49 +0530

    added more ingo

--- index.php
+++ index.php
@@ -1,6 +1,40 @@
 <?php
+
+define("INSTANCE",     "https://dev72378.service-now.com");
+
+
+$instance_des = getInstnaceDetails(); 
+echo $instance_des;exit;
+$update_response = file_get_contents("php://input");
+$update = json_decode($update_response, true);
+
+if (isset($update["queryResult"]["action"])) {
+    processMessage($update);
+    $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
+   fwrite($myfile, $update["queryResult"]["action"]);
+    fclose($myfile);
+}else{
+     sendMessage(array(
+            "source" => $update["responseId"],
+            "fulfillmentText"=>"Hello from webhook",
+            "payload" => array(
+                "items"=>[
+                    array(
+                        "simpleResponse"=>
+                    array(
+                        "textToSpeech"=>"Bad request"
+                         )
+                    )
+                ],
+                ),
+           
+        ));
+}
+
 function processMessage($update) {
     if($update["queryResult"]["action"] == "instance-details"){
+		$instance_details = getInstnaceDetails();
+		
         sendMessage(array(
             "source" => $update["responseId"],
             "fulfillmentText"=>"Hello from webhook",
@@ -59,29 +93,61 @@ function processMessage($update) {
 function sendMessage($parameters) {
     echo json_encode($parameters);
 }
- 
-$update_response = file_get_contents("php://input");
-$update = json_decode($update_response, true);
-if (isset($update["queryResult"]["action"])) {
-    processMessage($update);
-    $myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-   fwrite($myfile, $update["queryResult"]["action"]);
-    fclose($myfile);
-}else{
-     sendMessage(array(
-            "source" => $update["responseId"],
-            "fulfillmentText"=>"Hello from webhook",
-            "payload" => array(
-                "items"=>[
-                    array(
-                        "simpleResponse"=>
-                    array(
-                        "textToSpeech"=>"Bad request"
-                         )
-                    )
-                ],
-                ),
-           
-        ));
+
+function getInstnaceDetails(){
+	$curl = curl_init();	
+	curl_setopt_array($curl, array(
+			  CURLOPT_URL => INSTANCE."/stats.do",
+			  CURLOPT_RETURNTRANSFER => true,
+			  CURLOPT_CUSTOMREQUEST => "GET"			  
+			));
+
+			$response = curl_exec($curl);
+			$err = curl_error($curl);
+			$instance_desc = "";	
+
+			curl_close($curl);
+
+			if ($err) {
+			  echo "cURL Error #:" . $err;
+			} else {			  
+			  $response_array = explode ("<br/>", $response); 			  
+			  $instance_name = "";
+			  $insance_version = ""; 
+			  $processing = 0; 
+			  $cancelled = 0;
+			  
+			  foreach($response_array as $row){
+				  if (strpos($row, 'Build name:') !== false){					  
+						  $row_array = explode(':',$row);
+						  $insance_version = $row_array[1];	
+						  continue;
+						  
+				  }else if (strpos($row, 'Instance name:') !== false){					  
+						  $row_array = explode(':',$row);
+						  $instance_name = $row_array[1];	
+						  continue;
+						  
+				  }else if (strpos($row, 'Processor transactions:') !== false){					  
+						  $row_array = explode(':',$row);
+						  $processing = $row_array[1];	
+						  continue;
+						  
+				  }else if (strpos($row, 'Cancelled transactions:') !== false){					  
+						  $row_array = explode(':',$row);
+						  $cancelled = $row_array[1];	
+						  continue;
+						  
+				  }				  
+				  	
+			  }//for
+			}
+			
+			//The build name is - London. Currently, total transcation processing are - 32 and cancelled - 0.
+			//$instance_desc = "The instance name is ".$instance_name." and the version is ".$insance_version." Currently, total ".$processing."  transactions  are processing and ".$cancelled." are cancelled;
+			
+			$instance_desc = "The instance name is".$instance_name." and the version is".$insance_version." . Currently, total".$processing." transactions are processing and".$cancelled." are cancelled.";
+			return $instance_desc;
 }
+
 ?>
